c75e1fbffc583972e9cf2b1dd5f91de61b3d6c5e
afsul <afsulult@gmail.com>
2023-06-06 11:28:02 +0530

    requirement text added

--- House Price Prediction/DeployML/settings.py
+++ House Price Prediction/DeployML/settings.py
@@ -120,7 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
